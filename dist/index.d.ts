15e721a1ad6b37d40e3dfd09adb1e4d364d019a5
Johnson, Brian <Brian.Johnson167@T-Mobile.com>
2017-06-04 13:39:11 -0700

    clean slate for refactor2

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { ModuleWithProviders } from '@angular/core';
-export * from './state-store.component';
-export * from './state-store.service';
-export declare class StateStoreModule {
-    static forRoot(): ModuleWithProviders;
-}
